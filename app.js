8e4b37914cbe0b26b2d893e94e85cf57bcf384be
Adnan Aga <adnanaga11@gmail.com>
2022-06-09 16:15:34 -0400

    No need for socket io

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+// const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
